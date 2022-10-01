2499780d9a38bc1482996421b029372b894edb1f
chnmertblgn <chnmertblgn@gmail.com>
2022-10-01 15:10:03 +0300

    Add

--- methods2/src/Main.java
+++ methods2/src/Main.java
@@ -3,9 +3,11 @@ public class Main {
         String message = "Bugün hava çok güzel.";
         int total = returnTotal(5, 6);
         String city = returnCity();
+        int total2 = returnTotal2(2, 3, 4, 5, 6);
 
         System.out.println(total);
         System.out.println(city);
+        System.out.println(total2);
     }
 
     public static void add() {
@@ -22,6 +24,16 @@ public class Main {
 
     public static int returnTotal(int number, int number2) {
         return number + number2;
+    }
+
+    public static int returnTotal2(int... numbers) {
+        int total = 0;
+
+        for (int number : numbers) {
+            total += number;
+        }
+
+        return total;
     }
 
     public static String returnCity() {
